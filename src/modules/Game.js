eb6f4d5130df57bd44df8195b3f686a971b419f2
Evan McLean <evanhughmclean@gmail.com>
2022-10-01 16:09:44 -0400

    Adds the necessary UI elements and full game loop

--- src/modules/Game.js
+++ src/modules/Game.js
@@ -4,15 +4,15 @@ import Ship from "./Ship";
 import UI from "./UI";
 
 const Game = () => {
-  // carrier (5), battleship (4), destroyer (3), submarine (3), patrol boat (2)
   const p1 = Player();
-  const p1Ships = [Ship(5), Ship(4), Ship(3), Ship(3), Ship(2)]; // array.shift();
+  const p1Ships = [Ship(5), Ship(4), Ship(3), Ship(3), Ship(2)];
   const p1Board = Gameboard();
   const p2 = Player();
   const p2Ships = [Ship(5), Ship(4), Ship(3), Ship(3), Ship(2)];
   const p2Board = Gameboard();
 
   const ui = UI();
+  ui.rotateButton();
 
   while (p2Ships.length > 0) {
     const x = Math.floor(Math.random() * 10);
@@ -24,11 +24,10 @@ const Game = () => {
       p2Ships.shift();
     }
   }
-  p1Board.placeShip(0, 0, Ship(5), true);
 
   ui.renderBoard("Human", p1Board, true);
   ui.renderBoard("AI", p2Board, true);
-  ui.attackBoardOnClick(p1, p2, p1Board, p2Board);
+  ui.placeShips(p1Ships, p1Board, p2Board, p1, p2);
   ui.playAgainButton();
 };
 export default Game;
--- src/modules/UI.js
+++ src/modules/UI.js
@@ -1,10 +1,9 @@
-import Game from "./Game";
-
-/* eslint-disable no-param-reassign */
 const UI = () => {
   const container = document.querySelector(".container");
   const modal = document.getElementById("gameOverModal");
   const gameOverContent = document.querySelector(".game-over-content");
+  const rotate = document.querySelector(".rotate");
+  const placeFleet = document.querySelector(".message");
 
   const renderBoard = (playerName, gameboard, shouldRender) => {
     const board = document.createElement("div");
@@ -50,6 +49,8 @@ const UI = () => {
     gameOverContent.classList.add("show-modal");
     if (humanPlayer) {
       message.textContent = "Congratulations! You won!";
+    } else {
+      message.textContent = "The computer has won this time...";
     }
   };
 
@@ -78,24 +79,54 @@ const UI = () => {
   };
 
   const playAgainButton = () => {
-    const button = document.querySelector('.play-again');
-    button.addEventListener('click', (e) => {
+    const button = document.querySelector(".play-again");
+    button.addEventListener("click", () => {
+      window.location.reload();
+    });
+  };
+
+  const rotateButton = () => {
+    rotate.addEventListener("click", (e) => {
       e.preventDefault();
-      const boards = document.querySelectorAll('.gameboard');
-      boards.forEach(board => {
-        board.remove();
-      })
-      modal.classList.remove('show-modal');
-      gameOverContent.classList.remove('show-modal');
-      Game();
-    })
-  }
+      rotate.classList.toggle("active");
+    });
+  };
 
-  const placeShips = () => {
-    //carrier (5), battleship (4), destroyer (3), submarine (3), patrol boat (2)
-  }
+  const placeShips = (shipsArray, gameboard1, gameboard2, player1, player2) => {
+    const boards = document.querySelectorAll(".gameboard");
+    const square = document.querySelectorAll(".Human");
+    square.forEach((element) => {
+      element.addEventListener("click", (e) => {
+        const dataRow = Number(e.target.dataset.row);
+        const dataCol = Number(e.target.dataset.col);
+        const isHorizontal = !rotate.classList.contains("active");
+        if (shipsArray.length > 0) {
+          gameboard1.placeShip(dataCol, dataRow, shipsArray[0], isHorizontal);
+          if (gameboard1.board[dataRow][dataCol] === shipsArray[0]) {
+            boards.forEach((board) => board.remove());
+            renderBoard("Human", gameboard1, true);
+            renderBoard("AI", gameboard2, true);
+            shipsArray.shift();
+            placeShips(shipsArray, gameboard1, gameboard2, player1, player2);
+          }
+        }
+        if (shipsArray.length === 0) {
+          attackBoardOnClick(player1, player2, gameboard1, gameboard2);
+          playAgainButton();
+          placeFleet.textContent = "Attack the enemy!";
+          rotate.style.display = "none";
+        }
+      });
+    });
+  };
 
-  return { renderBoard, attackBoardOnClick, playAgainButton, placeShips };
+  return {
+    renderBoard,
+    attackBoardOnClick,
+    playAgainButton,
+    placeShips,
+    rotateButton,
+  };
 };
 
 export default UI;
